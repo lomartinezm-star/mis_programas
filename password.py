834a9d277d0c247a823323140fbf251dc7bf7da8
Lester Martinez <lomartinezm@alumno.uspg.edu.gt>
2026-02-14 01:35:00 -0600

    bug corregido

--- password.py
+++ password.py
@@ -1,13 +1,6 @@
-# Programa con bug (igual al de la presentación)
-
 password = input("Clave: ")
 
-# Prints de debug
-print(f"DEBUG tipo: {type(password)}")
-print(f"DEBUG valor: [{password}]")
-print(f"DEBUG len: {len(password)}")
-
-if password == 1234:  # BUG: string vs int
+if password == "1234":
     print("Acceso concedido")
 else:
     print("Acceso denegado")
