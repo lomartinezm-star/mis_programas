5f18138cb3d775a5022828ed4a99cd426cf2da97
Lester Martinez <lomartinezm@alumno.uspg.edu.gt>
2026-03-07 09:18:35 -0600

    Primer commit tablas del 1 al 12 en Python

--- Adivina el numero.py
+++ Adivina el numero.py
@@ -11,8 +11,36 @@ while True:
         print("Mas bajo.")
     else:
         print(f"Correcto en " f" {intentos} intentos.")
-        if intentos <= intentos_maximos:
+    if intentos <= intentos_maximos:
+            print(f"Perdiste. El numero era {numero_secreto}.")
             break
-        else:
+    else:
             print(f"Perdiste. El numero era {numero_secreto}.") 
             break
+    
+   
+    
+
+
+import random
+
+numero_secreto = random.randint(1, 100)
+intentos_maximos = 7
+intentos = 0
+
+while True:
+    intento = int(input("Adivina el numero (1-100): "))
+    intentos += 1
+
+    if intento < numero_secreto:
+        print("Mas alto.")
+    elif intento > numero_secreto:
+        print("Mas bajo.")
+    else:
+        print(f"Correcto en {intentos} intentos.")
+        break
+
+    # 🔹 Validar si ya perdió
+    if intentos == intentos_maximos:
+        print(f"Perdiste. El numero era {numero_secreto}.")
+        break
